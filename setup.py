efd98531c1d5b05c3c0d681ccc938df08361262d
Jan Hlavacek <jhlavace@svsu.edu>
2020-03-22 00:43:53 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='canvas_utils',
+      version='0.1',
+      description='Some utilities for interacting with Canvas LMS',
+      url='https://github.com/lahvak/canvas_utils',
+      author='Jan Hlavacek',
+      author_email='jhlavace@svsu.edu',
+      license='Apache 2.0',
+      packages=['canvas_tools'],
+      zip_safe=False)
